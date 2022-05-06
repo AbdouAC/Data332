2565579dd8d5b139846aaaac9e221372ae5de7ce
AbdouAC <99286544+AbdouAC@users.noreply.github.com>
2022-05-05 20:05:34 -0500

    Update cabbageButterflyProject.r

--- cabbageButterflyProject.r
+++ cabbageButterflyProject.r
@@ -8,7 +8,12 @@ setwd("C:/Users/husky/OneDrive/Desktop/System Analysis and design")
 
 
 df_PierisData <- read_excel("CompletePierisData_2022-03-09.xlsx", sheet = 1) %>%
-  dplyr::rename("country"="dwc:country")
+  dplyr::rename("country"="dwc:country") %>% 
+  dplyr::rename("year"="dwc:year") %>% 
+  dplyr::rename("continent"="dwc:continent") %>% 
+  dplyr::rename("decimallatitude"="dwc:decimalLatitude") %>% 
+  dplyr::rename("decimallongitude"="dwc:decimalLongitude")
+
 df_CleanedData <- read_excel("Cleaned Data LWA .xlsx", sheet = 1)
 
 df <- df_PierisData %>% 
